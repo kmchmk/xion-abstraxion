5876f6dd7e5aaf64fa1a8aab23f12ac873bded4f
Chanaka Karunarathne <kmchmk@gmail.com>
2024-01-25 06:44:53 +0000

    Add counter example

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,211 @@
-import Image from "next/image";
+"use client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import {
+  Abstraxion,
+  useAbstraxionAccount,
+  useAbstraxionSigningClient,
+} from "@burnt-labs/abstraxion";
+import { Button } from "@burnt-labs/ui";
+import "@burnt-labs/ui/styles.css";
+import type { ExecuteResult } from "@cosmjs/cosmwasm-stargate";
 
-export default function Home() {
+// Example XION counter contract
+const deployedContractAddress = "xion1x3sxr4wmug78yha27p6wpftt848x4nf6nhg2hfjvk89u2v8qr4hqyxc8ud";
+
+export default function Page(): JSX.Element {
+  // Abstraxion hooks
+  const { data: account, isConnected } = useAbstraxionAccount();
+  const { client } = useAbstraxionSigningClient();
+
+  // General state hooks
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [executeResult, setExecuteResult] = useState<ExecuteResult | undefined>(
+    undefined
+  );
+
+  const blockExplorerUrl = `https://explorer.burnt.com/xion-testnet-1/tx/${executeResult?.transactionHash}`;
+
+  // useEffect(() => {
+  //   if (account) {
+  //     console.log("account", account);
+  //   }
+  // }, [account]);
+
+  // useEffect(() => {
+  //   console.log("isConnected", isConnected);
+  // }, [isConnected]);
+
+  useEffect(() => {
+    if (client) {
+      console.log("client", client);
+      console.log("client.granteeAddress", client.granteeAddress);
+      console.log("client.granterAddress", client.granterAddress);
+    }
+  }, [client]);
+
+  async function incrementCount() {
+    setLoading(true);
+    const msg = { increment: {} };
+
+    try {
+      const executionResponse = await client?.execute(
+        client.granterAddress,
+        deployedContractAddress,
+        msg,
+        "auto"
+      );
+      console.log("executionResponse", executionResponse);
+      setExecuteResult(executionResponse);
+      getCount();
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function getCount() {
+    setLoading(true);
+    const msg = { get_count: {} };
+
+    try {
+      const queryResponse = await client?.queryContractSmart(
+        deployedContractAddress,
+        msg
+      );
+      console.log("queryResponse", queryResponse);
+      alert(`Count: ${queryResponse.count}`);
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function getBalances() {
+    setLoading(true);
+
+    try {
+      const granteeBalance = await client?.getBalance(
+        client.granteeAddress,
+        "uxion"
+      );
+      console.log("granteeBalance", granteeBalance);
+
+      const granterBalance = await client?.getBalance(
+        client.granterAddress,
+        "uxion"
+      );
+
+      console.log("granterBalance", granterBalance);
+      alert(
+        `Grantee: ${granteeBalance?.amount} ${granteeBalance?.denom}\nGranter: ${granterBalance?.amount} ${granterBalance?.denom}`
+      );
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="m-auto flex min-h-screen max-w-xs flex-col items-center justify-center gap-4 p-4">
+      <h1 className="text-2xl font-bold tracking-tighter text-white">
+        COUNTER EXAMPLE
+      </h1>
+      <Button
+        fullWidth
+        onClick={() => {
+          setIsOpen(true);
+        }}
+        structure="base"
+      >
+        {account.bech32Address ? (
+          <div className="flex items-center justify-center">VIEW ACCOUNT</div>
+        ) : (
+          "CONNECT"
+        )}
+      </Button>
+      {client ? (
+        <Button
+          disabled={loading}
+          fullWidth
+          onClick={() => {
+            void incrementCount();
+          }}
+          structure="base"
+        >
+          {loading ? "LOADING..." : "Increment Count"}
+        </Button>
+      ) : null}
+      {client ? (
+        <Button
+          disabled={loading}
+          fullWidth
+          onClick={() => {
+            void getCount();
+          }}
+          structure="base"
+        >
+          {loading ? "LOADING..." : "Get Count"}
+        </Button>
+      ) : null}
+      {client ? (
+        <Button
+          disabled={loading}
+          fullWidth
+          onClick={() => {
+            void getBalances();
+          }}
+          structure="base"
+        >
+          {loading ? "LOADING..." : "Get Balances"}
+        </Button>
+      ) : null}
+      <Abstraxion
+        isOpen={isOpen}
+        onClose={() => {
+          setIsOpen(false);
+        }}
+      />
+      {client && (
+        <div className="flex flex-col gap-4 w-full">
+          <div className="flex items-center justify-between">
+            <span className="text-sm font-medium text-gray-600 dark:text-gray-400">
+              Grantee:
+            </span>
+            <span className="text-sm font-mono text-gray-800 dark:text-gray-200">
+              {client.granteeAddress}
+            </span>
+          </div>
+          <div className="flex items-center justify-between">
+            <span className="text-sm font-medium text-gray-600 dark:text-gray-400">
+              Granter:
+            </span>
+            <span className="text-sm font-mono text-gray-800 dark:text-gray-200">
+              {client.granterAddress}
+            </span>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )}
+      {executeResult ? (
+        <div className="flex flex-col rounded border-2 border-black p-2 dark:border-white">
+          <div className="mt-2">
+            <p className="text-zinc-500">
+              <span className="font-bold">Transaction Hash</span>
+            </p>
+            <Link
+              className="text-black underline visited:text-purple-600 dark:text-white"
+              href={blockExplorerUrl}
+              target="_blank"
+            >
+              {executeResult.transactionHash}
+            </Link>
+          </div>
+        </div>
+      ) : null}
     </main>
   );
 }
